59d54539f3d139c320c692d253f3222479e6ed7c
omaks45 <danielgozie5@gmail..com>
2026-08-23 11:25:12 +0100

    module for quote request

--- src/modules/quote-requests/quote-requests.module.ts
+++ src/modules/quote-requests/quote-requests.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuoteRequestsController } from './quote-requests.controller';
 import { QuoteRequestsService } from './quote-requests.service';
 import { AuthModule } from '../auth/auth.module';
+import { ClientsModule } from '../clients/clients.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ClientsModule],
   controllers: [QuoteRequestsController],
   providers: [QuoteRequestsService],
 })
